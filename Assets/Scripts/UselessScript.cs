db3541cf6de21706127c56993d5130c29eeacb61
Jesse Aaltonen <jesse.aaltonen@metropolia.fi>
2026-09-18 20:22:52 +0300

    added a useless script

---
+++ Assets/Scripts/UselessScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UselessScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("this script is useless");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
